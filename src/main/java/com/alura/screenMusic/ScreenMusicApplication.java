f37a528b0bc35a7261b305b69eee0e1f97eda94c
Arthur Arraes <109257634+arthurarraes@users.noreply.github.com>
2025-02-10 08:27:18 -0300

    Inicialização do projeto

---
+++ src/main/java/com/alura/screenMusic/ScreenMusicApplication.java
@@ -0,0 +1,13 @@
+package com.alura.screenMusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenMusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alura/screenMusic/ScreenMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.screenMusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
